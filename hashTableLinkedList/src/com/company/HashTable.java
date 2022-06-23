e9359b74e3a5d164ba20f1bcee0c747e42a91061
summercrawford <sdc1579@gmail.com>
2022-06-23 18:14:37 -0500

    Started creating my hash table. Can now put elements in the hash table (untested).

---
+++ hashTableLinkedList/src/com/company/HashTable.java
@@ -0,0 +1,63 @@
+package com.company;
+
+// class for executing the hash table commands and whatnot
+// I'm just gonna leave the load factor at 0.5
+// Keys are integers and values are integers.
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class HashTable {
+    // the number of slots in the hash table
+    private int capacity;
+    // the number of elements in the hash table
+    private int elements;
+    // array of linked lists (to store collisions)
+    List<LinkedList<Integer>> indices;
+
+    // default constructor, capacity = 4
+    public HashTable() {
+        capacity = 4;
+        indices = new ArrayList<LinkedList<Integer>>();
+        // no elements
+        elements = 0;
+    }
+
+    // constructor with a given capacity
+    public HashTable(int givenCapacity) {
+        // assign the capacity to the given capacity
+        capacity = givenCapacity;
+        // create the array list
+        indices = new ArrayList<LinkedList<Integer>>();
+        // no elements
+        elements = 0;
+    }
+
+    // this function is meant to return the hash of a key
+    // or just modulo'ing the key with the capacity of the table
+    public int computeHash(int key) {
+        return key % capacity;
+    }
+
+    // adding an element to the hash table
+    public void put(int key, int value) {
+        // hash the key and use that as an index
+        int index = computeHash(key);
+        // get the linked list at a specific index
+        LinkedList<Integer> list = indices.get(index);
+        // add that value to the linked list
+        list.add(value);
+    }
+
+    // print the hash table
+    public void print() {
+        
+    }
+
+    // I know I'm gonna need to empty all the linked lists and collapse the table.
+    public void emptyTable() {
+        // do the emptying
+        // gonna have to iterate through linked list at each index and empty them
+    }
+}
--- hashTableLinkedList/src/com/company/Main.java
+++ hashTableLinkedList/src/com/company/Main.java
@@ -1,5 +1,12 @@
 package com.company;
 
+// This will be personal implementation of a hash table (based off of CTCI docs - page 90)
+// Okay I'm not going to do a full Java implementation of a linked list, I'll use Java's class.
+// TOMORROW, I will be doing my own implementation of a linked list.
+// It's fun how I can just keep working backwards with things haha.
+
+import java.util.LinkedList;
+
 public class Main {
 
     public static void main(String[] args) {
